fd89a40cb6462fbae68546100cc534288e05a3ea
Rubens Machion <iotti.rubin@gmail.com>
2021-08-12 11:54:38 -0300

    Update Package description

--- Package.swift
+++ Package.swift
@@ -7,8 +7,11 @@ let package = Package(
     name: "pagarme-swift-ios",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "pagarme-swift-ios", targets: ["pagarme-swift-ios", "RSA"]),
-        .library(name: "pagarme-swift-ios-static", type: .static, targets: ["pagarme-swift-ios", "RSA"]),
+        .library(name: "pagarme-swift-ios",
+                 targets: ["pagarme-swift-ios", "RSA"]),
+        .library(name: "pagarme-swift-ios-static",
+                 type: .static,
+                 targets: ["pagarme-swift-ios", "RSA"]),
     ],
     dependencies: [],
     targets: [
